62fd3cc4f0de7c512b917bed9996fbb2ba49623b
cchannawebdeveloper <cchanna.y.oeurng@gmail.com>
2023-02-18 18:26:24 +0700

    my first message

---
+++ src/main/java/com/heroku/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.heroku;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringBootHerokuChanna2Application.class);
+	}
+
+}
